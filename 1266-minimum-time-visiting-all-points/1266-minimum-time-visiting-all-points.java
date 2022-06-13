a570709ccf46a93b081fa38afc63ebefc3fe2e74
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2022-06-13 06:38:22 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (95.91%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -3,9 +3,7 @@ class Solution {
        
         int ans=0;
         for(int i=1;i<points.length;i++){
-             int []current = points[i];
-             int []previous = points[i-1];
-            ans+=Math.max(Math.abs(current[1]-previous[1]),Math.abs(current[0]-previous[0]));
+            ans+=Math.max(Math.abs(points[i][1]-points[i-1][1]),Math.abs(points[i][0]-points[i-1][0]));
         }
         return ans;
     }
