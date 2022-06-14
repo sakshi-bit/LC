961ab5ea2a513d9769f73b98377cc3985a8a54e9
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2022-06-14 07:35:06 +0530

    Time: 5 ms (13.15%), Space: 5.9 MB (84.88%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int> v;
-        while(num){
-            v.insert(v.begin(),num%10);
-            num/=10;
-        }
-        
-        for(int i=0;i<v.size();i++){
-            if(v[i]==6){
-                v[i]=9;
+        string s = to_string(num);
+        for(auto &c: s){
+            if(c=='6'){
+                c='9';
                 break;
             }
         }
-        int result = 0;
-        for (auto d : v)  
-        {
-           result = result * 10 + d;
-        }
-        return result;
+        return stoi(s);
     }
 };
