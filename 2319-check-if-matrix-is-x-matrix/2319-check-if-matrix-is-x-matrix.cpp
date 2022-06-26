86af90ef58cfa3d3f914f05d897d8806f90d3eb2
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2022-06-27 04:28:25 +0530

    Time: 48 ms (33.33%), Space: 16.3 MB (33.33%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     bool checkXMatrix(vector<vector<int>>& grid) {
+        bool flag=1;
         int n = grid.size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(i==j || (i+j)==n-1){
-                    if(grid[i][j]==0){
-                        return false;
-                    }
-                }else{
+            for(int j=0;j<n;j++){
+               if(i==j){
+                   if(grid[i][j]>0){
+                       flag =1;
+                   }else{
+                       flag = 0;
+                       return false;
+                   }
+               }else
+                
+                if((i+j)==n-1){
                     if(grid[i][j]>0){
+                        flag=1;
+                    }else{
+                        flag=0;
                         return false;
                     }
                 }
-            }
+                
+                else{
+                    if(grid[i][j]!=0){
+                        flag=0;
+                        return false;
+                    }
+                }
+               
+                
             
         }
+    }
         return true;
     }
 };
