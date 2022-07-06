cc7f5f3172f093eafe670fd825f3c3a7883661ca
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2022-07-06 13:12:06 +0530

    Time: 19 ms (35.48%), Space: 7.2 MB (77.95%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())
-            return false;
-        int n = s.length();
-        int anagrams[26]={0};
-        for(int i=0;i<n;i++){
-            anagrams[s[i]-'a']++;
-            anagrams[t[i]-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(anagrams[i])
-                return false;
-        }
-        
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
