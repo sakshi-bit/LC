84e4ff1c0c003eebf3f6630113e5f20101134fc2
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2022-07-13 14:52:08 +0530

    Time: 12 ms (69.33%), Space: 14.7 MB (5.45%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
         int row = image.size();
         int col = image[0].size();
         
-        int og_color = image[sr][sc];
+        int original_color = image[sr][sc];
         
-        if(og_color == newColor){
+        //step1 whether the original color is only the new color the we don't have to do anything
+        
+        if(original_color == color){
             return image;
         }
         
-        image[sr][sc] = newColor;
-        if(sr > 0 && image[sr-1][sc] == og_color){
-            floodFill(image,sr-1,sc,newColor);
+        //recursive solution
+        
+        //first assign the color to the source box
+        
+        image[sr][sc] = color;
+        
+        
+        //when we go in up direction
+        if(sr>0 && image[sr-1][sc]==original_color){
+            floodFill(image,sr-1,sc,color);
         }
         
-        if(sc>0 && image[sr][sc-1]==og_color){
-            floodFill(image,sr,sc-1,newColor);
+        //when we go in left direction
+        if(sc>0 && image[sr][sc-1]==original_color){
+             floodFill(image,sr,sc-1,color);
         }
         
-        if(sr<row-1 && image[sr+1][sc]==og_color){
-            floodFill(image,sr+1,sc,newColor);
+        //when we go in down direction
+        if(sr<row-1 && image[sr+1][sc]==original_color){
+            floodFill(image,sr+1,sc,color);
         }
         
-        if(sc<col-1 && image[sr][sc+1] == og_color){
-            floodFill(image,sr,sc+1,newColor);
+        //when we go right direction
+        if(sc<col-1 && image[sr][sc+1] == original_color){
+            floodFill(image,sr,sc+1,color);
         }
         
         return image;
